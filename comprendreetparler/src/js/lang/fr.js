15bb131e217b3ecf03721d7e9cadf0abb7188149
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-04-16 15:23:49 +0200

    create language pages

---
+++ comprendreetparler/src/js/lang/fr.js
@@ -0,0 +1,7 @@
+export default{
+        message: {
+            centre: 'Centre',
+            asbl: 'ASBL'
+        }
+      
+}
---
+++ comprendreetparler/src/js/lang/nl.js
@@ -0,0 +1,6 @@
+export default{
+    message: {
+        centre: 'Centrum',
+        asbl: 'VZW'
+    }
+}
