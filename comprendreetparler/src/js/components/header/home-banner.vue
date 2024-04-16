e08583589ad5d42be957a008ed7c0e6c8eeaf6ce
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-04-16 16:24:40 +0200

    Update home-banner.vue

--- comprendreetparler/src/js/components/header/home-banner.vue
+++ comprendreetparler/src/js/components/header/home-banner.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <h1 class="fade-appear">
-        <span class="special-font">{{ $t("message.centre") }}</span>
+        <span class="special-font">{{ $t("banner.centre") }}</span>
         <br>
         Comprendre et parler
         <br>
-        <span class="special-font">{{ $t("message.asbl") }}</span>
+        <span class="special-font">{{ $t("banner.asbl") }}</span>
         </h1>
     </div>
 </template>
