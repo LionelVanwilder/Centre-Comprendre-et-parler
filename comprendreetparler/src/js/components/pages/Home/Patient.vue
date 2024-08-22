b33d49b030390c46f36be930780f757520895248
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-08-22 19:21:37 +0200

    Update Patient.vue

--- comprendreetparler/src/js/components/pages/Home/Patient.vue
+++ comprendreetparler/src/js/components/pages/Home/Patient.vue
@@ -1,20 +1,14 @@
 <template>
     <div class="image-text-row patient-section" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-bottom">
         <div class="image-wrapper">
-            <img src="../../../../../images/patient.webp" alt="">
+            <img src="../../../../../images/patient.webp" alt="Patient enfant">
         </div>
         <div class="content-wrapper">
             <div class="content-wrapper-content patient-content"  data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="center-bottom">
                 <img :src="iconpath" alt="" class="icon-bloc-flex">
                 <h2 v-html="$t('patient.title')"></h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-                    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-                    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                    mollit anim id est laborum.
+                    Le Centre Comprendre et Parler accueille des enfants sourds ou malentendants, offrant un soutien spécialisé pour leur développement du langage et de la communication. Les patients bénéficient de thérapies adaptées, visant à améliorer leur compréhension et expression orale, ainsi que leur intégration sociale. Le centre s'engage à fournir des solutions personnalisées pour aider chaque enfant à s’épanouir pleinement et à mieux interagir avec son environnement.
                 </p>
             </div>
         </div>
@@ -34,3 +28,8 @@
 
     
 </script>
+<style scoped>
+.patient-content p{
+    text-align: justify;
+}
+</style>
