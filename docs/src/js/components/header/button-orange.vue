e74073315da42f03cced5fc3262c7399126c7559
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-08-28 12:56:18 +0200

    remove docs

--- docs/src/js/components/header/button-orange.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <button><slot /></button>
-</template>
-
-<script>
-    export default{
-        name: 'ButtonOrange'
-    }
-</script>
--- docs/src/js/components/navbar/button.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <router-link to="/Don" class="navbuttoncomp">{{ $t("navbar.navbutton") }}</router-link>
-</template>
-
-<script>
-    export default{
-        name: 'NavButton',
-    }
-</script>
