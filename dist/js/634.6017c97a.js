0d96e6b86ad81b692d8e76f6a1a3f5e55aa72258
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-08-29 18:49:19 +0200

    Deploy new build

---
+++ dist/js/634.6017c97a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcomprendreetparler"]=self["webpackChunkcomprendreetparler"]||[]).push([[634],{3634:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=634.6017c97a.js.map
