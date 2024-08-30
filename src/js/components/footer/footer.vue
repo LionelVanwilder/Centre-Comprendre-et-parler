ebb7d0ed7ce22d48be8553af2442ffe4932039e7
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-08-30 10:20:08 +0200

    Update footer.vue

--- src/js/components/footer/footer.vue
+++ src/js/components/footer/footer.vue
@@ -4,36 +4,36 @@
             <h4>Contact</h4>
             <ul>
                 <li><a href="#">02 770 04 40</a></li>
-                <li><a href="#">adresseMail@outlook.com</a></li>
-                <li><a href="#">{{ $t("banner.adress") }}</a></li>
+                <li><a href="mailto:philippine.dedonnea@ccpasbl.be">philippine.dedonnea@ccpasbl.be</a></li>
+                <li><a href="https://www.google.com/maps/place//data=!4m2!3m1!1s0x47c3dc9b63dbd941:0xeb71a7aeb16468?sa=X&ved=2ahUKEwjjutrVudGEAxWIm_0HHfJLDe8Q4kB6BAgOEAA" target="_blank">{{ $t("banner.adress") }}</a></li>
             </ul>
         </div>
         <div class="container-media-footer">
             <h4>{{ $t("footer.follow") }}</h4>
             <div class="media-footer">
-                <a href="#">Facebook</a>
+                <a href="https://www.facebook.com/CentreComprendreEtParlerAsbl/?locale=fr_FR" target="_blank">Facebook</a>
             </div>
-            <div class="media-footer">
+            <!--<div class="media-footer">
                 <a href="#">Instagram</a>
-            </div>
+            </div>-->
             <div class="media-footer">
-                <a href="#">Linkedin</a>
+                <a href="https://fr.linkedin.com/company/comprendre-et-parler" target="_blank">Linkedin</a>
             </div>
         </div>
     
         <div class="privacy">
             <ul>
                 <li><a href="#">{{ $t("footer.terms") }}</a></li>
-                <li><a href="#">{{ $t("footer.gdpr") }}</a></li>
+                <!--<li><a href="#">{{ $t("footer.gdpr") }}</a></li>-->
                 <li><a href="#">{{ $t("footer.legal") }}</a></li>
-                <li><a href="#">Cookies</a></li>
+                <!--<li><a href="#">Cookies</a></li>-->
             </ul>
         </div>
     </div>
         
     <div class="footer-footer">
         <div class="policy">
-            <a>{{ $t("footer.rights") }}</a>
+            <p>{{ $t("footer.rights") }}</p>
         </div>
     </div>
 </template>
@@ -43,3 +43,11 @@
         name: 'FooterApp'
     }
 </script>
+
+<style scoped>
+footer p{
+    color: #ffffff;
+    letter-spacing: 1.2px;
+    text-transform: capitalize;
+}
+</style>
