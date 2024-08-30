86fe2012ae53602cbbc26000a6aa4d927a345f69
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-08-30 21:49:14 +0200

    trad med

--- src/js/components/pages/competences/medecins/medecins.vue
+++ src/js/components/pages/competences/medecins/medecins.vue
@@ -2,37 +2,17 @@
     <div class="template-page-container">
         <BreadCrumbCom :breadcrumbs="breadcrumbs"></BreadCrumbCom>
     <div class="Edu-container">
-        <h2>Médecins - ORL</h2>
+        <h2>{{ $t('skills.med.title') }}</h2>
         <p>
-            Le médecin-directeur organise et anime des réunions interdisciplinaires de réflexion et de concertation sur les méthodes, 
-            et prend la décision finale de modification ou d'introduction de nouvelles méthodes. Il dirige et supervise individuellement, 
-            les activités des différents ré-éducateurs, et participe aux réunions d'équipe des différents secteurs.
+            {{ $t('skills.med.content1') }}
         </p>
         <p>
-            Il intervient directement auprès des enfants et de leurs parents, des adolescents ou adultes en début de processus de 
-            rééducation, et entretient des contacts personnels avec eux tout au long du processus. Il  veille à établir et à développer 
-            des contacts avec la communauté des sourds adultes, afin d'obtenir leur adhésion aux programmes de rééducation du Centre et leur 
-            coopération dans le domaine de l'utilisation des signes. Il assure les contacts avec les médecins extérieurs appelés à intervenir 
-            auprès des patients du Centre soit complémentairement au processus de rééducation (chirurgiens-électrophysiologistes) soit pour 
-            des affections autres que la déficience auditive.
+            {{ $t('skills.med.content2') }}
         </p>
         <p>
-            Il examine les rapports préparés par ses collaborateurs et rédige avec leur aide les rapports médicaux destinés à différents 
-            organismes (INAMI – Allocations familiales – FCRSPPH) ou aux médecins traitants, et entretient des rapports suivis avec ces 
-            derniers ainsi qu'avec les différents spécialistes appelés à intervenir pour des pathologies non liées à la déficience auditive.
-            Les médecins ORL effectuent les examens nécessaires à l'établissement ou à la confirmation du diagnostic des nouveaux patients 
-            adressés au Centre.
+            {{ $t('skills.med.content3') }}
         </p>
-        <p>
-            Ils prescrivent les examens complémentaires et traitements appropriés, en concertation avec les autres membres de l'équipe 
-            interdisciplinaire. Lorsqu'un traitement chirurgical devant être effectué en milieu hospitalier est nécessaire, ils 
-            effectuent celui-ci eux-mêmes  en dehors du Centre (activité hors convention) ou adressent les patients à d'autres spécialistes 
-            extérieurs.
-        </p>
-        <p>
-            Ils examinent régulièrement les patients en rééducation: ceux-ci requièrent effectivement une attention toute particulière à la 
-            sphère ORL, afin de prévenir ou de remédier rapidement à toute détérioration de leur audition résiduelle.
-        </p>
+       
     </div>
     </div>
     
