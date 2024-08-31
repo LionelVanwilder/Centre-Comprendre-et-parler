834a99f75ad432bea68582ade0d358fa87dc7acf
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-08-31 10:28:42 +0200

    trad psychomot

--- src/js/components/pages/competences/psychomotricite/psychomotricite.vue
+++ src/js/components/pages/competences/psychomotricite/psychomotricite.vue
@@ -2,35 +2,21 @@
     <div class="template-page-container">
         <BreadCrumbCom :breadcrumbs="breadcrumbs"></BreadCrumbCom>
     <div class="Edu-container">
-        <h2>Psychomotricité</h2>
+        <h2>{{ $t('skills.psychomot.title') }}</h2>
         <p>
-            Une partie des thérapies psychomotrices et des thérapies du développement est effectuée par des psychomotricien(ne)s ayant une 
-            formation particulière pour ce type de thérapies. Les enfants à audition déficiente en âge de rééducation précoce  ou d'école 
-            maternelle ont des difficultés particulières découlant de leur déficience auditive dans le développement psychomoteur général ou 
-            en raison de fréquents troubles de l'équilibre associé à la déficience auditive (par une atteinte vestibulaire) et d'une mauvaise 
-            perception des rythmes et de l'espace.
+            {{ $t('skills.psychomot.content1') }}
         </p>
         <p>
-            Les enfants à audition déficiente en âge de rééducation précoce ou d'école maternelle présentent aussi des troubles particuliers 
-            d'adaptation aux relations et activités de groupe. C'est pourquoi ces enfants requièrent soit des séances de thérapie psychomotrice 
-            individuelle, soit des séances collectives axées les unes sur le développement psychomoteur général, les autres sur la rééducation 
-            psychomotrice relationnelle.
+            {{ $t('skills.psychomot.content2') }}
         </p>
         <p>
-            Le rôle du psychomotricien est de chercher à modifier l’attitude de l’enfant sourd par rapport à son corps pour tenter d’établir, 
-            de rétablir, de maintenir et d’enrichir ses rapports avec lui-même, avec autrui et avec son environnement. Le champ d’application 
-            comprend pour l’essentiel les troubles qui s’expriment par des perturbations psychomotrices d’une part, et ceux pour lesquels 
-            l’approche du corps paraît la seule possible ou la meilleure à un moment précis d’autre part.
+            {{ $t('skills.psychomot.content3') }}
         </p>
         <p>
-            Le rôle du psychomotricien réside dans l’attention qu’il porte aux manifestations corporelles et à leurs significations, ainsi que 
-            dans l’établissement d’un dialogue corporel.
+            {{ $t('skills.psychomot.content4') }}
         </p>
         <p>
-            Le psychomotricien soutient l’enfant dans la mise en place des repères instrumentaux ainsi que dans la construction de son unité 
-            corporelle et psychique, afin d’établir des bases solides indispensables aux apprentissages et au développement harmonieux de 
-            l’enfant.  Cette mission s'inscrit dans celle plus générale du centre Comprendre et Parler qui vise à développer les potentialités 
-            et les compétences linguistiques de l'enfant sourd, ainsi que de favoriser son épanouissement personnel et son autonomie.
+            {{ $t('skills.psychomot.content5') }}
         </p>
     </div>
     </div>
