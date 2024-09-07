f1010a6c17d476c7f0d1d962b549081c0b00325e
LionelVanwilder <lionel.vanwilder@std.heh.be>
2024-09-07 19:59:00 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,5 @@
-const PrerenderPlugin = require('prerender-spa-plugin');
-const path = require('path');
-const Renderer = PrerenderPlugin.PuppeteerRenderer;
-
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new PrerenderPlugin({
-        staticDir: path.join(__dirname, 'dist'),
-        routes: ['/'],
-        renderer: new Renderer({
-          headless: true,
-          renderAfterDocumentEvent: 'render-event',
-        }),
-      }),
-    ],
-  },
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/',
+})
